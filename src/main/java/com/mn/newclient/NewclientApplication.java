8fee2ed52b815eddee9f8b6a38cba3cbbb26d1fc
Marilia Nunes Alves <nunes.marilia@gmail.com>
2020-09-08 20:44:10 -0300

    Initial commit

---
+++ src/main/java/com/mn/newclient/NewclientApplication.java
@@ -0,0 +1,13 @@
+package com.mn.newclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mn/newclient/NewclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mn.newclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
